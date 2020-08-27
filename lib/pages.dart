deab4f5c992af8b96119a2dcb686e19fc8a7a391
Nathan <kulewoshienathan@gmail.com>
2020-08-27 20:33:49 +0000

    pages changes

--- lib/pages.dart
+++ lib/pages.dart
@@ -48,21 +48,6 @@ class Pages extends StatelessWidget {
             ),
           ),
           Divider(),
-          InkWell(
-            onTap: () => Navigator.push(context,
-                MaterialPageRoute(builder: (BuildContext context) => Specs())),
-            child: Container(
-              width: MediaQuery.of(context).size.width,
-              padding: EdgeInsets.symmetric(vertical: 20),
-              child: Center(
-                child: Text(
-                  "Specs Widget",
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-            ),
-          ),
-          Divider(),
         ],
       ),
     );
